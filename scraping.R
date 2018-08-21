8a3c875290efbf85bc6bf375468b1b7a84c38f46
Ansgar Wolsing <ansgar.wolsing@alvary.io>
2018-08-21 20:56:39 +0200

    added get_content

--- scraping.R
+++ scraping.R
@@ -1,5 +1,22 @@
-install.packages("rvest")
 
 library(tidyverse)
 library(rvest)
+
+url_vector <- c(base = "http://www.kicker.de/news/fussball/dfbpokal/spielrunde/dfb-pokal", 
+                season = "2017-18",
+                round = 1,
+                suffix = "0/spieltag.html"
+                )
+
+# retrieve content
+get_content <- function(url) {
+  if (is.vector(url)) {
+    url <- str_c(url, collapse = "/")
+  }
+  url %>%
+      read_html()
+}
+
+page <- get_content(url_vector)
+page
 
