07cb435959f4bc2629cb4d244166d4ac0a4461da
Ansgar Wolsing <ansgar.wolsing@alvary.io>
2018-08-22 20:41:47 +0200

    runnable scraper for dfb cup results

--- scraping.R
+++ scraping.R
@@ -1,7 +1,7 @@
 
 library(tidyverse)
 library(rvest)
-
+#library(data.table)
 
 # retrieve page content - takes either a URL string or a vector of strings which constitute a url (will be collapsed to one string using "/")
 get_content <- function(url) {
@@ -13,102 +13,34 @@ get_content <- function(url) {
 }
 
 
-# url_vector <- c(base = "http://www.kicker.de/news/fussball/dfbpokal/spielrunde/dfb-pokal", 
-#                 season = "2012-13",
-#                 round = 2, # value between 1 (1st round) and 6 (final)
-#                 suffix = "0/spieltag.html"
-#                 )
-# 
-# page <- get_content(url_vector)
-# page
-# 
-# saveRDS(page, file = "pagecontent.rds")
-# 
-# # extract result table using xpath and transform into data frame
-# result_table <- html_node(page, xpath = "//table[@class='tStat tabdfb-pokal']")
-# result_df <- html_table(result_table, fill = TRUE, header = TRUE) #use fill = TRUE due to inconsistent column numbers (at least in round 1)
-# 
-# result_df <- result_df[, c(3, 5, 6)]
-# 
-# dim(result_df)
-# colnames(result_df)
-# 
-# # change column names
-# colnames(result_df) <- c("home", "away", "result")
-# 
-# str(result_df)
-# 
-# # delete empty rows
-# result_df <- result_df %>%
-#   filter(result != "")
-# 
-# # check number of rows (=matches)
-# dim(result_df)
-# 
-# # format results
-# result_df <- result_df %>%
-#   mutate(
-#          season = url_vector["season"], #change in function
-#          round = url_vector["round"], #change in function
-#          result_cleaned = str_trim(str_replace_all(result, "(n\\.V\\.|i\\.E\\.|\\(\\d+:\\d+\\))", "")),
-#          extratime = ifelse(str_detect(result, "n.V."), TRUE, FALSE),
-#          penalties = ifelse(str_detect(result, "i.E."), TRUE, FALSE),
-#          home_goals = str_match(result_cleaned, "(\\d+):")[, 2],
-#          away_goals = str_match(result_cleaned, ":(\\d+)")[, 2],
-#          winner = as.factor(ifelse(home_goals > away_goals, "home", "away")) # there are no draws
-#   ) %>%
-#   select(season, round, everything()) #reorder: put season and round to the left
-#   
-# str(result_df)
-
-
-
-
-
 # need a list of teams in 1. and 2. Bundesliga for each season. Potential source: table of the respective season
   # alternative: get 3. Bundesliga as well. Then all remaining teams would be 4th division and below.
 
-
-
-scrape_round <- function(season, round = 1) {
-  if (!round %in% 1:6)  return(NULL)
+scrape_stage <- function(season, stage = 1) {
+  if (!stage %in% 1:6)  return(NULL)
   url_vector <- c(base = "http://www.kicker.de/news/fussball/dfbpokal/spielrunde/dfb-pokal", 
                   season = season,
-                  round = round, # value between 1 (1st round) and 6 (final)
+                  stage = stage, # value between 1 (1st stage) and 6 (final)
                   suffix = "0/spieltag.html"
-  )
+                  )
   
   page <- get_content(url_vector)
-  page
-  
-  saveRDS(page, file = "pagecontent.rds")
   
   # extract result table using xpath and transform into data frame
   result_table <- html_node(page, xpath = "//table[@class='tStat tabdfb-pokal']")
-  result_df <- html_table(result_table, fill = TRUE, header = TRUE) #use fill = TRUE due to inconsistent column numbers (at least in round 1)
+  result_df <- html_table(result_table, fill = TRUE, header = TRUE) #use fill = TRUE due to inconsistent column numbers (at least in stage 1)
   
   result_df <- result_df[, c(3, 5, 6)]
-  
-  dim(result_df)
-  colnames(result_df)
   
   # change column names
   colnames(result_df) <- c("home", "away", "result")
   
-  str(result_df)
-  
-  # delete empty rows
+  # delete empty rows and format results
   result_df <- result_df %>%
-    filter(result != "")
-  
-  # check number of rows (=matches)
-  dim(result_df)
-  
-  # format results
-  result_df <- result_df %>%
+    filter(result != "") %>%
     mutate(
       season = season,
-      round = round,
+      stage = stage,
       result_cleaned = str_trim(str_replace_all(result, "(n\\.V\\.|i\\.E\\.|\\(\\d+:\\d+\\))", "")),
       extratime = ifelse(str_detect(result, "n.V."), TRUE, FALSE),
       penalties = ifelse(str_detect(result, "i.E."), TRUE, FALSE),
@@ -116,13 +48,48 @@ scrape_round <- function(season, round = 1) {
       away_goals = str_match(result_cleaned, ":(\\d+)")[, 2],
       winner = as.factor(ifelse(home_goals > away_goals, "home", "away")) # there are no draws
     ) %>%
-    select(season, round, everything()) #reorder: put season and round to the left
+    select(season, stage, everything()) #reorder: put season and stage to the left
   
-  str(result_df)
-  
-  result_df  
+  as_tibble(result_df)
 }
 
-round6 <- scrape_round("2017-18", 6)
+
+scrape_season <- function(season) {
+  season_n <- length(season)
+  seasons <- vector("list", season_n)
+  stages <- vector("list", 6)
+  results <- map(seasons, function (x) x[[1]] <- stages) #broken
+  
+  for (s in 1:season_n) {
+    message(str_c("Iterating season ", season[s], "."))
+    for (stage in 1:6) {
+      message(str_c(" |__ Iterating stage ", stage, "."))
+      tryCatch(
+        results[[s]][[stage]] <- scrape_stage(season[s], stage),
+        error = function(e) return(data.frame())
+      )
+    }
+  }
+  results %>% purrr::flatten() %>% data.table::rbindlist() %>% as_tibble()
+}
+
+
+# create correct season format just by starting year
+format_year <- function(year) {
+  if(!is.numeric(year)) return(NULL)
+  year %>%
+    str_c("-", year %% 100 + 1) 
+}
+
+
+pokal <- scrape_season(format_year(1963:2017))
+pokal %>% count(season, stage)
+
+
+
+#library(microbenchmark)
+#microbenchmark(scrape_season(c("2016-17")))
+
+
 
 
