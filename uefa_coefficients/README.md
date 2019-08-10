9418def4f5f7a1f957fd50dc1b663b2f8fa2d3a6
Ansgar Wolsing <ansgar.wolsing@alvary.io>
2019-08-10 22:05:39 +0200

    added animations and readme

---
+++ uefa_coefficients/README.md
@@ -0,0 +1,19 @@
+UEFA coefficients animated
+==========================
+
+Data source
+-----------
+
+Country rankings and coefficients were scraped from
+<a href="https://kassiesa.home.xs4all.nl/bert/uefa/data/index.html" class="uri">https://kassiesa.home.xs4all.nl/bert/uefa/data/index.html</a>
+using the `rvest` package.
+
+Rankings and coefficients over time
+-----------------------------------
+
+Animated graphs were created using `gganimate`.
+
+Limited to countries which ever made it to the annual top 5 in order to
+reduce the number of items displayed on the chart.
+
+![](uefa_coefficients_ranks.gif)
--- uefa_coefficients/uefa_coefficients.R
+++ uefa_coefficients/uefa_coefficients.R
@@ -12,7 +12,6 @@ get_content <- function(url) {
   if (is.vector(url)) {
     url <- str_c(url, collapse = "/")
   }
-  #content <- read_html(url, options = c("RECOVER"))
   tryCatch({content <- read_html(url)}, 
            error = function(e) {
              warning(e)
@@ -28,7 +27,7 @@ scrape_uefa_coefficients <- function(year) {
   request_url <- str_c("https://de.competitions.uefa.com/memberassociations/uefarankings/country/libraries//years/", year)
   html_content <- get_content(request_url)
   t <- html_node(html_content, css = "table.table--standings") %>% 
-    html_table( fill = TRUE) %>%
+    html_table(fill = TRUE) %>%
     select(country = Land, position = Pos, coefficient = `Pkt.`, no_of_teams = Vereine) %>%
     mutate(country = str_trim(str_sub(country, 5, (str_length(country) - 5) / 2 + 3)), # remove prefix, and keep only the first of the duplicated country names
            position = as.numeric(position),
@@ -40,89 +39,45 @@ scrape_uefa_coefficients <- function(year) {
 }
 
 
-coefficients_tbl <- map_dfr(c(1997:2003, 2005:2019), scrape_uefa_coefficients) %>% # 2004 data is missing on the UEFA website
+# scraping coefficients from https://kassiesa.home.xs4all.nl/
+# Historical coefficients from 1960 onwards
+scrape_uefa_coefficients_2 <- function(year) {
+  if (year < 1960 | year > 2019) {
+    warning(sprintf("Year %.0f is outside the scope of the data source, returning NA.", year))
+    return(NA)
+  } else if (year >= 1960 & year <= 1998) {
+    method <- 1
+  } else if (year >= 1999 & year <= 2003) {
+    method <- 2
+  } else if (year >= 2004 & year <= 2008) {
+    method <- 3
+  } else if (year >= 2009 & year <= 2017) {
+    method <- 4
+  } else {
+    method <- 5
+  }
+  request_url <- str_c("https://kassiesa.home.xs4all.nl/bert/uefa/data/", 
+               "method", method, "/",
+               "crank", year,".html"
+               )
+  html_content <- get_content(request_url)
+  t <- html_node(html_content, css = "table.t1") %>% 
+    html_table(fill = TRUE)
+  names(t) <- c("rank_no", "X1", "country",
+                "prev_year_4", "prev_year_3", "prev_year_2", "prev_year_1", "current_year",
+                "coefficient", "no_of_teams")
+  t <- t %>%
+    select(-X1) %>%
+    cbind(year)
+  t              
+}
+
+
+#########
+
+coefficients_tbl <- map_dfr(1960:2019, scrape_uefa_coefficients_2) %>% # 2004 data is missing on the UEFA website
   as_tibble()
 
-
-## EXPLORATION: UEFA coefficients
-
-# there is an odd dip in (mean) coefficients between 2005 and 2007 - standardize coefficients in order to avoid artefacts
-coefficients_tbl %>%
-  group_by(year) %>%
-  summarize(coeff_mean = mean(coefficient),
-            coeff_sd = sd(coefficient)) %>%
-  ggplot(aes(year, coeff_mean)) +
-  geom_line()
-
-# z-standardize coefficients by year
-coefficients_tbl <- coefficients_tbl %>%
-  group_by(year) %>%
-  mutate(coefficient_z = scale(coefficient)) %>%
-  ungroup()
-
-# check if z-standardization worked (mean = 0, sd = 1)
-coefficients_tbl %>%
-  group_by(year) %>%
-  summarize(coeff_z_mean = round(mean(coefficient_z)),
-            coeff_z_sd = round(sd(coefficient_z)))
-
-coefficients_tbl %>%  
-  filter(country %in% c("Spanien", "Deutschland", "England", "Italien", "Frankreich")) %>%
-  ggplot(aes(year, coefficient_z, col = country)) +
-  geom_line() +
-  coord_cartesian(ylim = c(0, max(coefficients_tbl$coefficient_z) + 1)) + 
-  theme_hc() + scale_color_hc()
-
-
-# prepare data frame for animation -> create a rank variable
-coefficients_ordered <- coefficients_tbl %>%
-  mutate(country = ifelse(country == "Tschechische Rep.", "Tschechien", country)) %>%
-  group_by(year) %>%
-  mutate(rank_no = rank(-coefficient, ties = "first")) %>%
-  ungroup() %>%
-  arrange(year, rank_no) %>%
-  select(year, rank_no, country, everything())
-
-
-# how many countries to display on chart
-display_countries_n <- 10
-
-
-# English translations of country names
-countries <- coefficients_ordered %>% distinct(country) %>% arrange(country) %>% pull(country)
-(countries_n <- length(countries))
-
-countries_en <- c(
-  "Albania", "Andorra", "Armenia", "Azerbaidzhan", "Belarus",
-  "Belgium", "Bosnia-Herzegovina", "Bulgaria", "Denmark", "Germany",
-  "England", "Estonia", "Färöer", "Finland", "France",
-  "Georgia", "Gibraltar", "Greece", "Iceland", "Israel",
-  "Italy", "Kazakstan", "Kosovo", "Croatia", "Latvia", 
-  "Liechtenstein", "Lithania", "Luxembourg", "Malta", "Moldavia", 
-  "Montenegro", "Netherlands", "Northern_Ireland", "North_Mazedonia", "Norway",
-  "Austria", "Poland", "Portugal", "Ireland", "Romania", 
-  "Russia", "San Marino", "Scotland", "Sweden", "Switzerland",
-  "Serbia", "Slovakia", "Slovenia", "Spain", "Czech_Republic",
-  "Turkey", "Ukraine", "Hungary", "Wales", "Cyprus"
-)
-
-country_translations <- bind_cols(country_de = countries, country_en = countries_en)
-
-# add translations to coeefficients dataframe
-coefficients_ordered <- 
-  coefficients_ordered %>%
-  left_join(country_translations, by = c("country" = "country_de"))
-
-
-# fake it - data for 2004 is missing on UEFA website
-coefficients_ordered_2003 <- coefficients_ordered %>%
-  filter(year == 2003)
-
-coefficients_ordered_2004 <- coefficients_ordered_2003 %>%
-  mutate(year = 2004)
-
-coefficients_ordered_fixed <- coefficients_ordered %>%
-  bind_rows(coefficients_ordered_2004)
 
 # fonts
 library(extrafont)
@@ -130,20 +85,23 @@ font_import()
 loadfonts(device = "win")
 windowsFonts()
 
+# how many countries to display on chart
+display_countries_n <- 10
+
 # create graph
-p <- coefficients_ordered_fixed %>%
+p <- coefficients_tbl %>%
   filter(rank_no <= display_countries_n) %>%
-  ggplot(aes(rank_no, group = country_en, fill = country_en)) +
+  ggplot(aes(rank_no, group = country, fill = country)) +
   geom_tile(aes(y = coefficient/2, height = coefficient), 
             width = 0.8, color = NA, alpha = 0.7, show.legend = FALSE) +
-  geom_text(aes(label = str_c("  ", country_en)), y = 1, size = 5, vjust = 0.5, hjust = 0, parse = TRUE) +
+  geom_text(aes(label = str_c("  ", str_replace(country, " ", "-"))), y = 1, size = 5, vjust = 0.5, hjust = 0, parse = TRUE) +
   geom_text(aes(label = sprintf("%.2f", coefficient), x = rank_no, y = coefficient + 3), vjust = 0) +
   coord_flip(clip = "off") +
-  scale_x_reverse(breaks = coefficients_ordered_fixed$rank_no) +
+  scale_x_reverse(breaks = coefficients_tbl$rank_no) +
   labs(
     title = "UEFA Coefficients Ranking", 
     subtitle = paste("Top", display_countries_n, "Leagues", "({closest_state})"), 
-    caption = "Source: UEFA.com",
+    caption = "Source: kassiesa.home.xs4all.nl",
     y = NULL, x = NULL
   ) +
   scale_fill_viridis_d(option = "D") +
@@ -165,11 +123,85 @@ p <- coefficients_ordered_fixed %>%
   )
 
 # animation
-fps <- 12
-years_n <- (2019 - 1997 - 1)
+fps <- 4
+years_n <- (2019 - 1960)
 anim <- p + transition_states(year, wrap = FALSE) + ease_aes("linear") 
 animate(anim, nframes = years_n * fps, fps = fps, width = 800, height = 600, end_pause = 4 * fps, renderer = av_renderer())
-anim_save("uefa_coefficients.mp4")
+anim_save("uefa_coefficients_top10.mp4")
 
 
+# coefficients over time
+
+# select top 10 countries from 2019
+top10_2019 <- coefficients_tbl %>%
+  filter(year == 2019, rank_no <= 10) %>%
+  pull(country)
+top10_2019
+
+top10_ever <- coefficients_tbl %>%
+  filter(rank_no <= 10) %>%
+  distinct(country) %>%
+  pull(country)
+
+top5_ever <- coefficients_tbl %>%
+  filter(rank_no <= 5) %>%
+  distinct(country) %>%
+  pull(country)
+
+
+p2 <- coefficients_tbl %>%
+  filter(country %in% top10_2019) %>%
+  #filter(year >= 2017) %>%
+  ggplot(aes(year, coefficient, col = country)) +
+  geom_line(alpha = 0.2) +
+  geom_point(size = 3) +
+  scale_color_viridis_d(option = "D") +
+  labs(title = "") +
+  theme_hc()
+
+anim2 <- p2 + transition_reveal(year)
+animate(anim2, width = 800, height = 600, renderer = av_renderer())
+
+
+
+p3 <- coefficients_tbl %>%
+  filter(country %in% top5_ever) %>%
+  #filter(year == 2019) %>%
+  ggplot(aes(rank_no, coefficient, col = country)) +
+  geom_point(size = 10, alpha = 0.7) +
+  geom_point(size = 10, shape = 1) +
+  geom_text(aes(label = country, x = rank_no, y = coefficient + 5), angle = 90, hjust = 0) +
+  scale_color_viridis_d(option = "D") +
+  scale_fill_viridis_d(option = "D") +
+  scale_x_reverse(limits = c(25, 1), breaks = seq(1, 25, 1)) +
+  labs(
+    title = "UEFA Ranking and UEFA Coefficients",
+    subtitle = "(Year {closest_state})",
+    caption = "Based on countries which ever made it to the top 5.\nSource: kassiesa.home.xs4all.nl",
+    x = "Country rank", y = "UEFA coefficient"
+       ) +
+  guides(col = FALSE, fill = FALSE, size = FALSE) +
+  theme_hc() + 
+  theme(
+    text = element_text(family = "Nunito Sans"),
+    #axis.text.y = element_blank(), 
+    axis.ticks = element_blank(),
+    #panel.grid = element_blank(),
+    panel.grid.major.x = element_line(color = "#DDDDDD"),
+    #panel.grid.minor.x = element_line(color = "#DDDDDD"),
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    panel.border = element_blank(),
+    plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"),
+    plot.title = element_text(size = 25, face = "bold"),
+    plot.subtitle = element_text(size = 15, hjust = 0)
+  )
+
+p3
+
+anim3 <- p3 + transition_states(year, wrap = FALSE) + ease_aes("cubic-in-out") 
+animate(anim3, nframes = 240, width = 800, height = 600, renderer = av_renderer())
+anim_save("uefa_coefficients_ranks.mp4")
+animate(anim3, nframes = 240, width = 800, height = 600)
+anim_save("uefa_coefficients_ranks.gif")
 
