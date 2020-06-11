f8f9da82aa1dbb0a92690ff79922ca5cf064b460
Ansgar Wolsing <ansgar.wolsing@alvary.io>
2020-06-11 21:25:30 +0200

    scrape Bundesliga tables from dfb.de

---
+++ scrape_bundesliga_tables/R/scrape_dfb.R
@@ -0,0 +1,61 @@
+library(tidyverse)
+library(rvest)
+library(tictoc)
+
+
+# Scrape page IDs for season final tables from drop-down selection
+# The value of the options contains the season id and matchday id (last matchday of season) combined
+
+url <- "https://www.dfb.de/bundesliga/spieltagtabelle/"
+page <- read_html(url)
+ddoptions <- html_nodes(page, xpath = "//select[@id='seasons']/option")
+
+seasons_mapping <- tibble(
+  season = html_text(ddoptions),
+  id = html_attr(ddoptions, name = "value")
+) %>% 
+  separate(id, into = c("season_id", "matchday_id"), sep = "\\|") %>% 
+  arrange(season)
+
+# use season mapping to send request to retrieve each seasons final table
+# example url: https://www.dfb.de/bundesliga/spieltagtabelle/?spieledb_path=%2Fcompetitions%2F12%2Fseasons%2F17820%2Fmatchday%2F34
+
+scrape_season_table <- function(season, season_id, matchday_id) {
+  url <- sprintf("https://www.dfb.de/bundesliga/spieltagtabelle/?spieledb_path=%%2Fcompetitions%%2F12%%2Fseasons%%2F%s%%2Fmatchday%%2F%s", season_id, matchday_id)
+  page <- read_html(url)
+  
+  # extract result table using xpath and transform into data frame
+  result_table <- html_node(page, xpath = "//table[@class='data-table table-bordered']")
+  
+  # prepare table
+  df <- html_table(result_table, fill = TRUE, header = TRUE)
+  colnames(df) <- c("position", "X1", "team", "played", "won", "draw", "lost", "goals", "goal_diff", "points")
+  df <- df %>% 
+    select(-starts_with("X")) %>% 
+    separate(goals, into = c("goals", "goals_against"), sep = ":") %>% 
+    mutate(points_2pt = 2 * won + draw,
+           points_3pt = 3 * won + draw,
+           season = season,
+           start_year = str_split_fixed(season, "/", 2)[, 2],
+           start_year = as.numeric(start_year)) %>% 
+    select(season, start_year, everything())
+  df
+}
+
+# scrape all seasons at once and store them in a list (takes a while)
+tic()
+final_tables <- pmap(seasons_mapping, scrape_season_table)
+toc()
+
+# name the list items with season names
+final_tables <- final_tables %>% 
+  set_names(seasons_mapping$season)
+
+# remove current season from the list
+final_tables[[nrow(seasons_mapping)]] <- NULL
+
+write_rds(final_tables, "output/bundesliga_final_tables.RData", compress = "gz")
+
+# save as csv file
+final_tables_flat <- bind_rows(final_tables)
+write_csv(final_tables_flat, "output/bundesliga_final_tables.csv")
