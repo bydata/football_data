a78c8cca9ef429309357afb0eba0b5f3c51218be
bydata <ansgar.wolsing@gmail.com>
2022-10-07 21:58:25 +0200

    update npxG+xA

--- modeste/modeste.R
+++ modeste/modeste.R
@@ -65,4 +65,46 @@ buli_fw_standard %>%
     plot.title.position = "plot",
     plot.subtitle = element_textbox(width = 0.92)
   )
-ggsave(here(base_path, "modeste_target-receiving_vs_thread-2.png"), width = 6, height = 6)
+ggsave(here(base_path, "modeste_target-receiving_vs_npxG-2.png"), width = 6, height = 6)
+
+buli_fw_standard %>% 
+  inner_join(buli_fw_possession, by = c("Player", "Season_End_Year", "Squad", "Comp", "Pos")) %>% 
+  tibble() %>% 
+  select(Season_End_Year, Comp, Squad, Player, Pos, MP_Playing, Min_Playing, Gls, Ast, 
+         Gls_Per, Ast_Per,  `Att 3rd_Touches`, Targ_Receiving, npxG_Per, `npxG+xA_Per`) %>% 
+  mutate(Att_3rd_Touches_Per = `Att 3rd_Touches` / Min_Playing * 90,
+         Targ_Receiving_Per = Targ_Receiving / Min_Playing * 90) %>% 
+  ggplot(aes(Targ_Receiving_Per, `npxG+xA_Per`)) +
+  geom_point(aes(fill = Player %in% selected_players, color = Player %in% selected_players,
+                 size = Min_Playing), 
+             shape = 21) +
+  ggrepel::geom_text_repel(
+    data = ~subset(., Player %in% selected_players),
+    aes(label = Player), size = 2, family = "Roboto Condensed", fontface = "bold") +
+  scale_fill_manual(values = c("FALSE" = "grey68", "TRUE" = "#FDE100")) +
+  scale_color_manual(values = c("FALSE" = "white", "TRUE" = "grey8")) +
+  guides(fill = "none", color = "none") +
+  labs(
+    title = "Modeste yet to make an impact at Borussia Dortmund",
+    subtitle = sprintf(
+      "Non-penalty expected goals plus expected assists per 90 mins by the number of times a pass
+      was attempted to this player.
+      Bundesliga forwards with at least %s matches played 
+      in the 2022/2023 season (matchday 8)
+    ", min_played_threshold),
+    caption = "Source: FBRef. Visualisation: Ansgar Wolsing",
+    x = "Target receiving per 90 mins",
+    y = "npxG+xA per 90 mins",
+    size = "Total minutes played"
+  ) +
+  theme_minimal(base_family = "Roboto Condensed") +
+  theme(
+    plot.background = element_rect(color = "white", fill = "white"),
+    legend.position = "bottom",
+    text = element_text(color = "grey24", lineheight = 1.1),
+    axis.text = element_text(color = "grey24"),
+    plot.title = element_text(color = "grey2", face = "bold"),
+    plot.title.position = "plot",
+    plot.subtitle = element_textbox(width = 0.92)
+  )
+ggsave(here(base_path, "modeste_target-receiving_vs_npxG+xA.png"), width = 6, height = 6)
