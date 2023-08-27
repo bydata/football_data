b0136ec1d6be377b1dcb2f5f8761e979963ec61e
bydata <ansgar.wolsing@gmail.com>
2023-08-27 14:32:07 +0200

    changed BVB colours; underlined title with grid

--- bocbvb-marktwerte-2023/bocbvb-marktwerte.R
+++ bocbvb-marktwerte-2023/bocbvb-marktwerte.R
@@ -3,6 +3,7 @@ library(ggtext)
 library(here)
 library(treemapify)
 library(worldfootballR)
+library(grid)
 
 base_path <- "bocbvb-marktwerte-2023"
 
@@ -27,7 +28,8 @@ market_values %>%
   View()  
   
 
-
+ragg::agg_png(here(base_path, "marktwerte-bocbvb-2023.png"), 
+              width = 8, height = 6, units = "in", res = 300)
 market_values %>% 
   filter(squad %in% c("VfL Bochum", "Borussia Dortmund")) %>% 
   mutate(squad = factor(squad, levels = c("VfL Bochum", "Borussia Dortmund"))) %>% 
@@ -35,35 +37,39 @@ market_values %>%
   ggplot(aes(area = player_market_value_euro, subgroup = squad,
              subgroup2 = player_name,
              fill = squad, col = squad)) +
-  geom_treemap(col = "white", size = 1, radius = unit(1, "mm"),
-               start = "bottomright") +
+  geom_treemap(
+    size = 1, radius = unit(1, "mm"), start = "bottomright") +
   geom_treemap_subgroup_text(family = "Outfit SemiBold", grow = FALSE,
                              start = "bottomright") +
   geom_treemap_subgroup2_text(
-    family = "Outfit Light", place = "top", alpha = 0.8, size = 9, grow = FALSE, 
+    family = "Outfit", place = "top", alpha = 0.8, size = 9, grow = FALSE, 
     reflow = TRUE, start = "bottomright") +
   geom_treemap_subgroup_border(col = "white", size = 6, start = "bottomright") +
-  scale_fill_manual(values = c("#005CA9", "#FDE100")) +
-  scale_color_manual(values = c("white", "black")) +
+  scale_fill_manual(values = c("#005CA9", "black")) +
+  scale_color_manual(values = c("white", "#FDE100")) +
   guides(fill = "none", color = "none") +
   labs(
     title = glue::glue("Marktwerte <span style='color:#005CA9'>VfL Bochum</span>
-                       vs. <span style='color:#FDE100'>Borussia Dortmund</span>"),
+                       vs. <span style='color:black'>Borussia Dortmund</span>"),
     caption = "**Daten:** Transfermarkt.de (Stand 26.08.2023). **Visualisierung:** Ansgar Wolsing"
   ) +
   theme_minimal(base_family = "Outfit") +
   theme(
     plot.background = element_rect(color = "white", fill = "white"),
     legend.position = "top",
-    text = element_text(color = "grey8"),
+    text = element_text(color = "grey28"),
     plot.title = element_markdown(
       family = "Outfit SemiBold", hjust = 0.5, size = 16, lineheight = 1.2, 
-      angle = 1.5, margin = margin(t = 8, b = 0)),
+      angle = 0, margin = margin(t = 8, b = 4)),
     plot.caption = element_markdown(size = 7, lineheight = 1.2)
   )
-ggsave(here(base_path, "marktwerte-bocbvb-2023.png"), width = 8, height = 6)
+# underline BVB in title
+grid.lines(x = c(0.555, 0.8), y = c(0.935, 0.935),
+           gp = gpar(col = "#FDE100", lwd = 4))
+dev.off()
 
-
+ragg::agg_png(here(base_path, "marktwerte-bocbvb-2023-equal-values.png"), 
+              width = 8, height = 6, units = "in", res = 300)
 market_values %>% 
   filter(squad == "VfL Bochum" | (squad == "Borussia Dortmund" & 
                                     player_name %in% c("Nico Schlotterbeck", "Felix Nmecha"))) %>% 
@@ -72,28 +78,31 @@ market_values %>%
   ggplot(aes(area = player_market_value_euro, subgroup = squad,
              subgroup2 = player_name,
              fill = squad, col = squad)) +
-  geom_treemap(col = "white", size = 1, radius = unit(1, "mm")) +
+  geom_treemap(size = 1, radius = unit(1, "mm")) +
   geom_treemap_subgroup_text(family = "Outfit SemiBold", grow = FALSE) +
   geom_treemap_subgroup2_text(
     family = "Outfit Light", place = "top", alpha = 0.8, size = 9, grow = FALSE, 
     reflow = TRUE) +
   geom_treemap_subgroup_border(col = "white", size = 6) +
-  scale_fill_manual(values = c("#005CA9", "#FDE100")) +
-  scale_color_manual(values = c("white", "black")) +
+  scale_fill_manual(values = c("#005CA9", "black")) +
+  scale_color_manual(values = c("white", "#FDE100")) +
   guides(fill = "none", color = "none") +
   labs(
     title = glue::glue("Marktwerte <span style='color:#005CA9'>VfL Bochum</span>
-                       vs. <span style='color:#FDE100'>Borussia Dortmund</span>"),
+                       vs. <span style='color:black'>Borussia Dortmund</span>"),
     caption = "**Daten:** Transfermarkt.de (Stand 26.08.2023). **Visualisierung:** Ansgar Wolsing"
   ) +
   theme_minimal(base_family = "Outfit") +
   theme(
     plot.background = element_rect(color = "white", fill = "white"),
     legend.position = "top",
-    text = element_text(color = "grey8"),
+    text = element_text(color = "grey28"),
     plot.title = element_markdown(
       family = "Outfit SemiBold", hjust = 0.5, size = 16, lineheight = 1.2, 
-      angle = 1.5, margin = margin(t = 8, b = 0)),
+      angle = 0, margin = margin(t = 8, b = 0)),
     plot.caption = element_markdown(size = 7, lineheight = 1.2)
   )
-ggsave(here(base_path, "marktwerte-bocbvb-2023-equal-values.png"), width = 8, height = 6)
+# underline BVB in title
+grid.lines(x = c(0.555, 0.8), y = c(0.935, 0.935),
+           gp = gpar(col = "#FDE100", lwd = 4))
+dev.off()
